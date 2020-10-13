111ae9b795aca23d53f4f88b4f7d511a02caaa0f
Christopher <christopherstoy@gmail.com>
2020-10-13 19:08:07 +0000

    added one to many association between store and employee models, created employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,12 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Chris", last_name: "Toy", hourly_rate: 60)
+@store1.employees.create(first_name: "Mac", last_name: "Toy", hourly_rate: 60)
+@store2.employees.create(first_name: "Kirito", last_name: "Soa", hourly_rate: 60)
+@store2.employees.create(first_name: "Raine", last_name: "Paul", hourly_rate: 60)
+@store2.employees.create(first_name: "Naruto", last_name: "Uzumaki", hourly_rate: 60)
+@store2.employees.create(first_name: "Peter", last_name: "Parker", hourly_rate: 60)
+
