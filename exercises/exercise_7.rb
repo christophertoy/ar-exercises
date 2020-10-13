a937a8fba3a748b89ad1656da1a849427b13bdd7
Christopher <christopherstoy@gmail.com>
2020-10-13 19:40:54 +0000

    added validations for employees and stores

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,8 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Enter store name"
+@store_name = gets.chomp
+invalid_store = Store.create(name: @store_name)
+puts invalid_store.errors.full_messages
+
